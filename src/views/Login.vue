4e414fd0a68d3d8989552c3e6c6e31e6dcc829db
3017218159 <1010043642@qq.com>
2021-03-18 11:23:10 +0800

    feat: 修改格式

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,51 +4,123 @@
     <div class="user-status" v-if="users && !users.status">
       <h3>请选择你的身份</h3>
       <el-row>
-        <el-button type="primary" @click="setUserStatus('STUDENT')">学生</el-button>
-        <el-button type="warning" @click="setUserStatus('ENTERPRISE')">企业</el-button>
+        <el-button type="primary" @click="setUserStatus('STUDENT')"
+          >学生</el-button
+        >
+        <el-button type="warning" @click="setUserStatus('ENTERPRISE')"
+          >企业</el-button
+        >
       </el-row>
     </div>
     <el-tabs type="border-card" v-else>
       <!--登录 tab -->
       <el-tab-pane label="登录">
-        <el-form :model="loginForm" status-icon :rules="rules" ref="loginForm" label-width="100px" v-if="loginForm.type === 'PASS'">
+        <el-form
+          :model="loginForm"
+          status-icon
+          :rules="rules"
+          ref="loginForm"
+          label-width="100px"
+          v-if="loginForm.type === 'PASS'"
+        >
           <el-form-item label="手机号" prop="phoneNumber">
-            <el-input v-model="loginForm.phoneNumber" autocomplete="off" clearable></el-input>
+            <el-input
+              v-model="loginForm.phoneNumber"
+              autocomplete="off"
+              clearable
+            ></el-input>
           </el-form-item>
           <el-form-item label="密码" prop="password">
-            <el-input type="password" v-model="loginForm.password" autocomplete="off" show-password clearable></el-input>
+            <el-input
+              type="password"
+              v-model="loginForm.password"
+              autocomplete="off"
+              show-password
+              clearable
+            ></el-input>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" @click="login('loginForm')">登录</el-button>
-            <el-button @click="switchLoginType">{{loginForm.switchContent}}</el-button>
+            <el-button type="primary" @click="login('loginForm')"
+              >登录</el-button
+            >
+            <el-button @click="switchLoginType">{{
+              loginForm.switchContent
+            }}</el-button>
           </el-form-item>
         </el-form>
-        <el-form :model="loginForm" status-icon :rules="rules" ref="loginForm" label-width="100px" v-else>
+        <el-form
+          :model="loginForm"
+          status-icon
+          :rules="rules"
+          ref="loginForm"
+          label-width="100px"
+          v-else
+        >
           <el-form-item label="手机号" prop="phoneNumber">
-            <el-input v-model="loginForm.phoneNumber" autocomplete="off" clearable></el-input>
+            <el-input
+              v-model="loginForm.phoneNumber"
+              autocomplete="off"
+              clearable
+            ></el-input>
           </el-form-item>
           <el-form-item label="验证码" prop="code">
-            <el-input type="code" v-model="loginForm.code" autocomplete="off" style="width: 160px"></el-input>
-            <el-button @click.prevent="getCode(loginForm.phoneNumber, 'loginForm')" style="width: 110px">获取验证码</el-button>
+            <el-input
+              type="code"
+              v-model="loginForm.code"
+              autocomplete="off"
+              style="width: 160px"
+            ></el-input>
+            <el-button
+              @click.prevent="getCode(loginForm.phoneNumber, 'loginForm')"
+              style="width: 110px"
+              >获取验证码</el-button
+            >
           </el-form-item>
           <el-form-item>
             <el-button type="primary" @click="login">登录</el-button>
-            <el-button @click="switchLoginType">{{loginForm.switchContent}}</el-button>
+            <el-button @click="switchLoginType">{{
+              loginForm.switchContent
+            }}</el-button>
           </el-form-item>
         </el-form>
       </el-tab-pane>
       <!--注册 tab -->
       <el-tab-pane label="注册">
-        <el-form :model="registerForm" status-icon :rules="rules" ref="registerForm" label-width="100px">
+        <el-form
+          :model="registerForm"
+          status-icon
+          :rules="rules"
+          ref="registerForm"
+          label-width="100px"
+        >
           <el-form-item label="手机号" prop="phoneNumber">
-            <el-input v-model="registerForm.phoneNumber" autocomplete="off" clearable></el-input>
+            <el-input
+              v-model="registerForm.phoneNumber"
+              autocomplete="off"
+              clearable
+            ></el-input>
           </el-form-item>
           <el-form-item label="密码" prop="password">
-            <el-input type="password" v-model="registerForm.password" autocomplete="off" show-password clearable></el-input>
+            <el-input
+              type="password"
+              v-model="registerForm.password"
+              autocomplete="off"
+              show-password
+              clearable
+            ></el-input>
           </el-form-item>
           <el-form-item label="验证码" prop="code">
-            <el-input type="code" v-model="registerForm.code" autocomplete="off" style="width: 160px"></el-input>
-            <el-button @click.prevent="getCode(registerForm.phoneNumber, 'registerForm')" style="width: 110px">获取验证码</el-button>
+            <el-input
+              type="code"
+              v-model="registerForm.code"
+              autocomplete="off"
+              style="width: 160px"
+            ></el-input>
+            <el-button
+              @click.prevent="getCode(registerForm.phoneNumber, 'registerForm')"
+              style="width: 110px"
+              >获取验证码</el-button
+            >
           </el-form-item>
           <el-form-item>
             <el-button type="primary" @click="register">注册</el-button>
@@ -60,63 +132,57 @@
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import { mapState } from "vuex";
 // import axios from 'axios';
 
 export default {
   data() {
     const validatePhoneNumber = (rule, value, callback) => {
-      if (value === '') {
-        callback(new Error('请输入手机号'));
+      if (value === "") {
+        callback(new Error("请输入手机号"));
       } else {
         //验证手机号格式
-        const myreg=/^[1][3,4,5,6,7,8,9][0-9]{9}$/;
+        const myreg = /^[1][3,4,5,6,7,8,9][0-9]{9}$/;
         if (!myreg.test(value)) {
-          callback(new Error('手机号格式不正确'));
+          callback(new Error("手机号格式不正确"));
         } else {
           callback();
         }
       }
     };
     const validatePassword = (rule, value, callback) => {
-      if (value === '') {
-        callback(new Error('请输入密码'));
+      if (value === "") {
+        callback(new Error("请输入密码"));
       } else {
         callback();
       }
     };
     const validateCode = (rule, value, callback) => {
-      if (value === '') {
-        callback(new Error('请输入验证码'));
+      if (value === "") {
+        callback(new Error("请输入验证码"));
       } else {
         callback();
       }
     };
     return {
       loginForm: {
-        phoneNumber: '',
-        password: '',
-        code: '',
-        type: 'PASS',
-        switchContent: '短信验证码登录'
+        phoneNumber: "",
+        password: "",
+        code: "",
+        type: "PASS",
+        switchContent: "短信验证码登录",
       },
       registerForm: {
-        phoneNumber: '',
-        password: '',
-        code: ''
+        phoneNumber: "",
+        password: "",
+        code: "",
       },
       rules: {
-        phoneNumber: [
-          { validator: validatePhoneNumber, trigger: 'blur' }
-        ],
-        password: [
-          { validator: validatePassword, trigger: 'blur' }
-        ],
-        code: [
-          { validator: validateCode, trigger: 'blur' }
-        ]
-      }
-    }
+        phoneNumber: [{ validator: validatePhoneNumber, trigger: "blur" }],
+        password: [{ validator: validatePassword, trigger: "blur" }],
+        code: [{ validator: validateCode, trigger: "blur" }],
+      },
+    };
   },
   computed: {
     ...mapState({
@@ -125,7 +191,7 @@ export default {
   },
   methods: {
     login() {
-      this.$refs['loginForm'].validate((valid) => {
+      this.$refs["loginForm"].validate((valid) => {
         if (valid) {
           //在这里进行登录操作
           this.$store.dispatch("users/loginAsync");
@@ -135,77 +201,70 @@ export default {
       });
     },
     register() {
-      this.$refs['registerForm'].validate((valid) => {
+      this.$refs["registerForm"].validate((valid) => {
         if (valid) {
           //在这里进行注册操作
-          console.log('register');
+          console.log("register");
         } else {
           return false;
         }
       });
     },
     switchLoginType() {
-      if(this.loginForm.type === 'PASS'){
-        this.loginForm.type = 'CODE';
-        this.loginForm.switchContent = '密码登录';
+      if (this.loginForm.type === "PASS") {
+        this.loginForm.type = "CODE";
+        this.loginForm.switchContent = "密码登录";
       } else {
-        this.loginForm.type = 'PASS';
-        this.loginForm.switchContent = '短信验证码登录';
+        this.loginForm.type = "PASS";
+        this.loginForm.switchContent = "短信验证码登录";
       }
     },
     setUserStatus(status) {
       this.$store.commit("users/setStatus", status);
     },
     getCode(phoneNumber, formName) {
-      console.log(this.$refs[formName].validateField('phoneNumber'), phoneNumber, formName);
-      this.$refs[formName].validateField('phoneNumber', (msg) => {
+      console.log(
+        this.$refs[formName].validateField("phoneNumber"),
+        phoneNumber,
+        formName
+      );
+      this.$refs[formName].validateField("phoneNumber", (msg) => {
         // console.log(msg);
         if (!msg) {
           //在这里进行获取验证码操作
-          console.log('getCode', phoneNumber);
+          console.log("getCode", phoneNumber);
         } else {
           return false;
         }
       });
-      // const appkey = "0c892d86efa2f844";
-      // this.$http.jsonp('https://api.binstd.com/sms/send', {
-      //   params: {
-      //     mobile: phoneNumber,
-      //     content: '',
-      //     appkey: appkey
-      //   },
-      //   jsonp: 'onBack'
-      // }).then((res) => {
-      //   console.log(res); 
-      // });
-    }
+    },
   },
 };
 </script>
 
 <style lang="scss">
-  .login {
-    width: 100%;
-    height: 100vh;
-    position: relative;
+.login {
+  width: 100%;
+  height: 100vh;
+  position: relative;
 
-    img {
-      width: 300px;
-      height: 100px;
-    }
-    .el-tabs {
-      position: absolute;
+  img {
+    width: 300px;
+    height: 100px;
+  }
+  .el-tabs {
+    position: absolute;
+    margin-top: 20px;
+    width: 500px;
+    left: 50%;
+    margin-left: -250px;
+
+    .el-form {
       margin-top: 20px;
-      width: 500px;
-      left: 50%;
-      margin-left: -250px;
-
-      .el-form {
-        margin-top: 20px;
-      }
-      .el-form-item {
-        margin-right: 100px;
-      }
+    }
+    .el-form-item {
+      margin-right: 100px;
     }
   }
+}
 </style>
